cda788fd5b0d2b310d021af9af22ddf2451a77a7
ali <ali@gmail.com>
2025-01-29 21:40:08 +0330

    feat: add passport config

---
+++ src/config/passport.config.ts
@@ -0,0 +1,41 @@
+import { User } from "@prisma/client";
+import bcrypt from "bcryptjs";
+import passport from "passport";
+import { Strategy as LocalStrategy } from "passport-local";
+import { prisma } from "../utils/prisma-client.utils";
+
+passport.use(
+  new LocalStrategy(
+    { usernameField: "email" },
+    async (email, password, done) => {
+      try {
+        const user = await prisma.user.findUnique({ where: { email } });
+
+        if (!user) return done(null, false, { message: "User not found" });
+
+        const isMatch = await bcrypt.compare(password, user.password);
+        if (!isMatch)
+          return done(null, false, { message: "Incorrect password" });
+
+        return done(null, user);
+      } catch (err) {
+        return done(err);
+      }
+    }
+  )
+);
+
+passport.serializeUser((user, done) => {
+  done(null, (user as User).id);
+});
+
+passport.deserializeUser(async (id: number, done) => {
+  try {
+    const user = await prisma.user.findUnique({ where: { id } });
+    done(null, user);
+  } catch (err) {
+    done(err);
+  }
+});
+
+export default passport;
