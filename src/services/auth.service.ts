b4022273eb44c84c4eb8eca39894c044cb40a975
ali <ali@gmail.com>
2025-01-31 20:42:24 +0330

    feat: use createError in loginUser service

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -1,6 +1,7 @@
 import { User } from "@prisma/client";
 import bcrypt from "bcryptjs";
 import passport from "passport";
+import { createError } from "../utils/create-error";
 import { prisma } from "../utils/prisma-client.utils";
 
 export const registerUser = async (email: string, password: string) => {
@@ -21,10 +22,13 @@ export const loginUser = (email: string, password: string): Promise<User> => {
         info: { message: string }
       ) => {
         if (err) return reject(err);
-        if (!user) return reject(new Error(info.message));
+        if (!user)
+          return reject(
+            createError(info.message || "Invalid credentials", 401)
+          );
 
         resolve(user);
       }
-    )({ body: { email, password } } as any); // Simulating req.body to match passport's expectations
+    )({ body: { email, password } } as unknown as Request);
   });
 };
