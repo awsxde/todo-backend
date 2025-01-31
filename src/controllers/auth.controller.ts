04f07b00165ca3e63697d4ea047d7d50a3857f5c
ali <ali@gmail.com>
2025-01-31 20:42:08 +0330

    feat: use createError in login controller

--- src/controllers/auth.controller.ts
+++ src/controllers/auth.controller.ts
@@ -7,6 +7,7 @@ import {
 } from "../dtos/auth.dto";
 import { loginUser, registerUser } from "../services/auth.service";
 import catchAsync from "../utils/catch-async";
+import { createError } from "../utils/create-error";
 
 export const register = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
@@ -26,16 +27,16 @@ export const login = catchAsync(
   async (req: Request, res: Response, next: NextFunction): Promise<void> => {
     const requestDto: LoginUserRequestDto = req.body;
     const user = await loginUser(requestDto.email, requestDto.password);
+
+    if (!user) next(createError("Invalid email or password", 401));
+
     const responseDto: LoginUserResponseDto = {
       id: user.id,
       email: user.email,
       createdAt: user.createdAt.toISOString(),
     };
 
-    req.logIn(user, async (loginErr) => {
-      if (loginErr) return next(loginErr);
-      res.status(200).json(responseDto);
-    });
+    res.status(200).json(responseDto);
   }
 );
 
