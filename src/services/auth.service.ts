19a1a1194242d9519d779559649faabb276e8b4d
ali <ali@gmail.com>
2025-01-31 20:55:35 +0330

    fix: prevent creating duplicate user

--- src/services/auth.service.ts
+++ src/services/auth.service.ts
@@ -5,10 +5,14 @@ import { createError } from "../utils/create-error";
 import { prisma } from "../utils/prisma-client.utils";
 
 export const registerUser = async (email: string, password: string) => {
+  const existingUser = await prisma.user.findUnique({ where: { email } });
+  if (existingUser) throw createError("User already exists", 400);
+
   const hashedPassword = await bcrypt.hash(password, 10);
   const user = await prisma.user.create({
     data: { email, password: hashedPassword },
   });
+
   return user;
 };
 
